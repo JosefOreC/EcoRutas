19ebe3f57f6112239bc3a74b0d7f2df5e5bc1097
Josef Pablo Ore Campos <JosefOre2005@gmail.com>
2024-12-01 23:26:27 -0500

    Programas de prueba para EcoRutas

---
+++ src/main/java/Conexion/Conection.java
@@ -0,0 +1,28 @@
+package Conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conection {
+    private String url="jdbc:sqlserver://localhost:1433;databaseName=dbEcoRutas;encrypt=true;trustServerCertificate=true";
+    private String user="sa";
+    private String password="123456";
+    public Connection con = null;
+    public Conection(){
+        Conectarse();
+    }
+    public Connection Conectarse(){
+        try{
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            con=DriverManager.getConnection(url, user, password);
+            System.out.println("Se logró conexion");
+        }catch(ClassNotFoundException e){
+            System.out.println("No se logró conexion\n"+e);
+        }catch(SQLException e){
+            System.out.println("No se logró conexion\n"+e);
+        }
+        return con;
+    }
+}
+
